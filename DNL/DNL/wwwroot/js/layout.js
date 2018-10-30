1da202052d7afd75263e1749365ef762c4efb644
dnl-frontend <41146812+dnl-frontend@users.noreply.github.com>
2018-10-30 21:56:59 +0200

    fixed bug of dropdown menu on mobile phones

--- DNL/DNL/wwwroot/js/layout.js
+++ DNL/DNL/wwwroot/js/layout.js
@@ -1,13 +1,19 @@
 ﻿/*Dropdown menu*/
-$('body').on('mouseenter mouseleave', '.navbar .dropdown', function (e) {
-    var dropdown = $(e.target).closest('.navbar .dropdown');
-    var menu = $('.dropdown-menu', dropdown);
-    dropdown.addClass('show');
-    menu.addClass('show');
-    setTimeout(function () {
-        dropdown[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
-        menu[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
-    }, 100);
+$(document).ready(function () {
+    if ($(window).width() >= '992') {
+        $('body').on('mouseenter mouseleave', '.navbar .dropdown', function (e) {
+
+            var dropdown = $(e.target).closest('.navbar .dropdown');
+            var menu = $('.dropdown-menu', dropdown);
+            dropdown.addClass('show');
+            menu.addClass('show');
+            setTimeout(function () {
+                dropdown[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
+                menu[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 100);
+
+        });
+    }
 });
 
 $(function () {
